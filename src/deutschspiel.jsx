eedfdfad3729f8f8abdb030191bc4a64ed855c0f
fcobonilla0902 <fcobonilla0902@gmail.com>
2026-01-31 17:55:38 -0600

    update

--- src/deutschspiel.jsx
+++ src/deutschspiel.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { useState, useEffect, useRef, useCallback } from "react";
 
 
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Deutschspiel from "./deutschspiel.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Deutschspiel />
+  </React.StrictMode>
+);
